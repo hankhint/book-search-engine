5a4d2045c8a27face7cc54e5e492826ad42c468a
HankHint <hankhint@gmail.com>
2022-03-08 16:04:23 -0600

    strange git behavior after renaming folder

--- server/server.js
+++ server/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
+
+//import apollo server and auth middleware
+const { ApolloServer } = require("apollo-server-express")
+const { authMiddleware } = require("./utils/auth")
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 
+//disabled routes
+//const routes = require('./routes');
+
+//keep app and PORT as-is
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//create Apollo server
+//Apollo server gets our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+})
+
+//Apollo and Express integration as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +32,15 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//disable routes
+//app.use(routes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build"));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+  console.log(`🌍 Now listening on localhost:${PORT}`)
+  console.log(`graphQL at http://localhost:${PORT}${server.graphqlPath}`)
+  });
 });
