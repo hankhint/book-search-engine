753361f1e7aeb6decf2e481298d22a0e0962e925
HankHint <hankhint@gmail.com>
2022-03-08 17:08:50 -0600

    added saveBook and removeBook and disabled mutations for thoughts

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -40,9 +40,9 @@ const resolvers = {
   Mutation: {
     addUser: async (parent, args) => {
       try {
-      const user = await User.create(args);
-      const token = signToken(user);
-      return { token, user };
+        const user = await User.create(args);
+        const token = signToken(user);
+        return { token, user };
       } catch (err) {
         console.log(err);
       }
@@ -63,52 +63,76 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-    addThought: async (parent, args, context) => {
-      if (context.user) {
-        const thought = await Thought.create({
-          ...args,
-          username: context.user.username,
-        });
+    //   addThought: async (parent, args, context) => {
+    //     if (context.user) {
+    //       const thought = await Thought.create({
+    //         ...args,
+    //         username: context.user.username,
+    //       });
 
-        await User.findByIdAndUpdate(
+    //       await User.findByIdAndUpdate(
+    //         { _id: context.user._id },
+    //         { $push: { thoughts: thought._id } },
+    //         { new: true }
+    //       );
+
+    //       return thought;
+    //     }
+
+    //     throw new AuthenticationError("You need to be logged in!");
+    //   },
+    //   addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+    //     if (context.user) {
+    //       const updatedThought = await Thought.findOneAndUpdate(
+    //         { _id: thoughtId },
+    //         {
+    //           $push: {
+    //             reactions: { reactionBody, username: context.user.username },
+    //           },
+    //         },
+    //         { new: true, runValidators: true }
+    //       );
+
+    //       return updatedThought;
+    //     }
+
+    //     throw new AuthenticationError("You need to be logged in!");
+    //   },
+    //   addFriend: async (parent, { friendId }, context) => {
+    //     if (context.user) {
+    //       const updatedUser = await User.findOneAndUpdate(
+    //         { _id: context.user._id },
+    //         { $addToSet: { friends: friendId } },
+    //         { new: true }
+    //       ).populate("friends");
+
+    //       return updatedUser;
+    //     }
+
+    //     throw new AuthenticationError("You need to be logged in!");
+    //   },
+    // },
+
+    saveBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
           { _id: context.user._id },
-          { $push: { thoughts: thought._id } },
-          { new: true }
-        );
-
-        return thought;
-      }
-
-      throw new AuthenticationError("You need to be logged in!");
-    },
-    addReaction: async (parent, { thoughtId, reactionBody }, context) => {
-      if (context.user) {
-        const updatedThought = await Thought.findOneAndUpdate(
-          { _id: thoughtId },
-          {
-            $push: {
-              reactions: { reactionBody, username: context.user.username },
-            },
-          },
+          { $addToSet: { savedBooks: args.input } },
           { new: true, runValidators: true }
         );
-
-        return updatedThought;
+        return updatedUser;
       }
-
       throw new AuthenticationError("You need to be logged in!");
     },
-    addFriend: async (parent, { friendId }, context) => {
+    removeBook: async (parent, args, context) => {
       if (context.user) {
         const updatedUser = await User.findOneAndUpdate(
           { _id: context.user._id },
-          { $addToSet: { friends: friendId } },
+          { $pull: { savedBooks: { bookId: args.bookId } } },
           { new: true }
-        ).populate("friends");
-
+        );
         return updatedUser;
       }
-
       throw new AuthenticationError("You need to be logged in!");
     },
   },
