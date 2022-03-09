ed3f055ea5593fbdb5bbebcb60cfa40f97d70351
HankHint <hankhint@gmail.com>
2022-03-08 23:16:14 -0600

    updated components, pages, and utils to use mutations and queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -80,3 +80,43 @@ export const REMOVE_FRIEND = gql`
     }
   }
 `;
+
+export const SAVE_BOOK = gql `
+mutation saveBook($input: savedBook!) {
+    saveBook (input: $input) {
+        _id
+        username 
+        email
+        bookCount
+        savedBooks {
+            #_id
+            bookId
+            authors
+            image 
+            link
+            title 
+            description
+        }
+    }
+}
+`;
+
+export const REMOVE_BOOK = gql `
+mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+        _id
+        username 
+        email
+        bookCount
+        savedBooks {
+            #_id
+            bookId
+            authors
+            image 
+            link
+            title 
+            description
+        }
+    }
+}
+`;
