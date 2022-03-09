27bb5a489b0a5c2d288aa234db0ede6b987a6a0b
HankHint <hankhint@gmail.com>
2022-03-08 22:22:56 -0600

    changing api and auth javascript files in the client directory

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,41 +1,92 @@
-const jwt = require("jsonwebtoken");
+//const jwt = require("jsonwebtoken");
 
-const secret = "mysecretsshhhhh";
-const expiration = "2h";
+// const secret = "mysecretsshhhhh";
+// const expiration = "2h";
 
-module.exports = {
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+// module.exports = {
+//   signToken: function ({ username, email, _id }) {
+//     const payload = { username, email, _id };
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+//     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+//   },
 
-  authMiddleware: function ({ req }) {
-    //allows token to be sent via req.body, req.query, or headers
+//   authMiddleware: function ({ req }) {
+//     //allows token to be sent via req.body, req.query, or headers
 
-    let token = req.body.token || req.query.token || req.headers.authorization;
-    // let token = req.query.token || req.headers.authorization;
-    console.log("TOKEN", token);
-    //separate "Bearer" and "<tokenvalue>"
-    if (req.headers.authorization) {
-      token = token.split(" ").pop().trim();
-    }
+//     let token = req.body.token || req.query.token || req.headers.authorization;
+//     // let token = req.query.token || req.headers.authorization;
+//     console.log("TOKEN", token);
+//     //separate "Bearer" and "<tokenvalue>"
+//     if (req.headers.authorization) {
+//       token = token.split(" ").pop().trim();
+//     }
 
-    //if no token, return request original object
-    if (!token) {
-      return req;
-    }
+//     //if no token, return request original object
+//     if (!token) {
+//       return req;
+//     }
 
+//     try {
+//       //decode data
+//       //attach decoded data to user object
+//       const { data } = jwt.verify(token, secret, { maxAge: expiration });
+//       console.log("DATA", data);
+//       req.user = data;
+//     } catch {  console.log("Invalid token");
+//     }
+//     //return updated request object
+//     return req;
+//   },
+// };
+// use this to decode a token and get the user's information out of it
+import decode from 'jwt-decode';
+
+// make a new class to instantiate a user
+class AuthService {
+  // get that user's data
+  getProfile() {
+    return decode(
+      this.getToken());
+  }
+
+  // check if that user's logged in
+  loggedIn() {
+    // Checks if there is a saved token and verifies the token is still valid
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token); 
+  }
+
+  // check if token is still valid
+  isTokenExpired(token) {
     try {
-      //decode data
-      //attach decoded data to user object
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      console.log("DATA", data);
-      req.user = data;
-    } catch {  console.log("Invalid token");
+      const decoded = decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } 
+      else return false;
+    } 
+    catch (err) {
+      return false;
     }
+  }
 
-    //return updated request object
-    return req;
-  },
-};
+  getToken() {
+    //gets user token from localStorage
+    return localStorage.getItem('id_token');
+  }
+
+  login(idToken) {
+    // S aves user token to localStorage
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+  logout() {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem('id_token');
+    // this will reload the page and reset the state of the application
+    window.location.assign('/');
+  }
+}
+
+export default new AuthService();
